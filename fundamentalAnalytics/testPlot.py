7d71ebcd3ed9affc6f027a5114e052deed6bc4af
afunes <afunes@SISTEMAS-029.ar.lumina.net>
2018-09-02 09:20:18 -0500

    BASE 9

--- fundamentalAnalytics/testPlot.py
+++ fundamentalAnalytics/testPlot.py
@@ -14,9 +14,8 @@ import plotly.graph_objs as go
 import plotly.plotly as py
 
 
-def getTrace(data, fileName, ticker, indicatorID):
-    rs = DaoCompanyResult.getCompanyResult(None, ticker, indicatorID)
-    concept = GenericDao.getFirstResult(Concept, Concept.conceptID == indicatorID)
+def getTraceData(ticker, concept, periodType):
+    rs = DaoCompanyResult.getCompanyResult(ticker = ticker, conceptID = concept.conceptID, periodType = periodType)
     rows = rs.fetchall()
     if (len(rows) != 0):
         df = DataFrame(rows)
@@ -25,17 +24,30 @@ def getTrace(data, fileName, ticker, indicatorID):
             x=df["yq"],
             y=df["value"],
             name = concept.label)
-        data.append(trace)
-        fileName = fileName + " " + concept.label
-
+        return trace
+    else:
+        raise Exception("No data found " + concept.conceptID)
 
 data = []   
 ticker = 'INTC'
 filename = ticker
+periodType = "YTD"
 Initializer()
-#listIndicatorID = ['SalesRevenueNet', 'CostOfGoodsAndServicesSold', 'GrossProfit']
-listIndicatorID = ['OperatingExpenses', 'OperatingIncomeLoss', 'NetIncomeLoss', 'CashAndCashEquivalentsPeriodIncreaseDecrease', 'IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest']
-#listIndicatorID = ['NetCashProvidedByUsedInOperatingActivitiesContinuingOperations', 'NetCashProvidedByUsedInInvestingActivitiesContinuingOperations','NetCashProvidedByUsedInFinancingActivitiesContinuingOperations']
-for indicatorID in listIndicatorID:
-    getTrace(data, filename, ticker, indicatorID)    
-py.iplot(data, filename = filename[0:100])
+#listConceptID = ['NetIncomeLoss']
+#BALANCE
+#listConceptID = ['CashAndCashEquivalentsAtCarryingValue', 'AssetsCurrent', 'Assets', 'LiabilitiesCurrent', 'StockholdersEquity']
+#listConceptID = ['OperatingExpenses', 'OperatingIncomeLoss', 'NetIncomeLoss', 'CashAndCashEquivalentsPeriodIncreaseDecrease', 'IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest']
+#CASH FLOW
+listConceptID = ['NetCashProvidedByUsedInOperatingActivitiesContinuingOperations', 'NetCashProvidedByUsedInInvestingActivitiesContinuingOperations','NetCashProvidedByUsedInFinancingActivitiesContinuingOperations']
+
+#listConceptID = ['NetCashProvidedByUsedInOperatingActivities', 'NetCashProvidedByUsedInInvestingActivities','NetCashProvidedByUsedInFinancingActivities']
+for conceptID in listConceptID:
+    concept = GenericDao.getFirstResult(Concept, Concept.conceptID == conceptID)
+    data.append(getTraceData(ticker, concept, periodType))
+    filename = filename + " " + concept.label
+print(filename)   
+layout = go.Layout(
+    title=filename[0:100]
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename = filename[0:100])
