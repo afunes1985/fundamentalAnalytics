a0d97d7b987173dabcd016ebc5ce69a5fef320cb
afunes <afunes@SISTEMAS044.ar.lumina.net>
2019-06-22 20:22:22 -0500

    Change datatable to original table

--- fundamentalAnalytics/web/apps/app1.py
+++ fundamentalAnalytics/web/apps/app1.py
@@ -7,59 +7,76 @@ from dao.dao import Dao
 from dao.factDao import FactDao
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
+import dash_table as dt
 from engine.plotlyEngineInterface import PlotlyEngineInterface
-from testPlot import testPlot
 from valueobject.valueobject import FilterFactVO
 from web.app import app
-
 
 Initializer()
 rs = Dao.getCompanyList()
 rows = rs.fetchall()
 if (len(rows) != 0):
     df = DataFrame(rows)
-    df.columns = rs.keys()
-layout = html.Div([
-    dt.DataTable(
-            rows=df.to_dict("rows"),
-            row_selectable=True,
-            filterable=True,
-            sortable=True,
-            selected_row_indices=[],
-            editable = False,
-            min_height = 200,
-            id='datatable-companyList'
-    ),
-    html.Div(id='datatable-companyList-container'),
+    df.columns = rs.keys() 
+    layout = html.Div([dt.DataTable(
+                id='dt-companyList',
+                columns=[
+                    {"name": i, "id": i, "deletable": False} for i in df.columns
+                ],
+                data=df.to_dict("rows"),
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                row_selectable="multi",
+                page_current= 0,
+                page_size= 10,
+        ),
+    html.Div(id='dt-companyList-container'),
     html.Button(id='btn-submit-showFact', n_clicks=0, children='Submit'),
-    html.Div(dt.DataTable(rows=[{}], id='datatable-factList'), style={'display': 'none'}),
-    html.Div(id='datatable-factList-container'),
+    html.Div(dt.DataTable(data=[{}], id='dt-factList'), style={'display': 'none'}),
+    html.Div(id='dt-factList-container'),
     html.Button(id='btn-submit-sendPlotlyData', n_clicks=0, children='Submit'),
     dcc.Link('Go to Fact Import', href='/apps/app2'),
     html.Div(id='hidden-div', style={'display':'none'})
 ])
 
 @app.callback(
-    Output('datatable-factList-container', "children"),
+    Output('dt-factList-container', "children"),
     [Input('btn-submit-showFact', 'n_clicks')],
-    [State('datatable-companyList', "rows"),
-     State('datatable-companyList', "selected_row_indices")])
-def doSubmitShowFact(n_clicks, rows, selected_row_indices):
-    if (selected_row_indices is not None and len(selected_row_indices) != 0):
-        for selected_row in selected_row_indices:
+    [State('dt-companyList', "derived_virtual_data"),
+     State('dt-companyList', "derived_virtual_selected_rows")])
+def doSubmitShowFact(n_clicks, rows, selected_rows):
+    if (selected_rows is not None and len(selected_rows) != 0):
+        for selected_row in selected_rows:
             df2 = getFactValues(rows[selected_row]["CIK"], rows[selected_row]["ticker"], None)
             dt2 = dt.DataTable(
-                rows=df2.to_dict("rows"),
-                row_selectable=True,
-                filterable=True,
-                sortable=True,
-                selected_row_indices=[],
-                id='datatable-factList',
-                min_width = 3500,
-                min_height = 500,
-                editable = False,
-                column_widths = {0 : 300, 1 : 300})
+                id='dt-factList',
+                columns=[
+                    {"name": i, "id": i, "deletable": False} for i in df2.columns
+                ],
+                data=df2.to_dict("rows"),
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                row_selectable="multi",
+                style_table={'overflowX': 'scroll'},
+                fixed_rows={ 'headers': True, 'data': 0 },
+                style_cell={
+                    'minWidth': '90px', 'maxWidth': '220px',
+                    'whiteSpace': 'no-wrap',
+                    'textOverflow': 'ellipsis',
+                    'overflow': 'hidden',
+                },
+                css=[{
+                    'selector': '.dash-cell div.dash-cell-value',
+                    'rule': 'display: inline; white-space: inherit; overflow: inherit; text-overflow: inherit;'
+                }],
+                style_cell_conditional=[
+                     {'if': {'column_id': 'reportName'},
+                         'textAlign': 'left'},
+                     {'if': {'column_id': 'conceptName'},
+                         'textAlign': 'left'}
+                 ])
             return dt2
  
 def getFactValues(CIK, ticker, conceptName2):
@@ -76,6 +93,7 @@ def getFactValues(CIK, ticker, conceptName2):
         reportDate = row[4]
         periodType = row[5]
         order = row[6]
+        #print(conceptName + " " + str(value) + " " + str(reportDate))
         if(rowDict.get('conceptName', None) != conceptName or rowDict.get('periodType', None) != periodType):
             if(rowDict.get('conceptName', None) is not None):
                 rows_list.append(rowDict)
@@ -102,12 +120,12 @@ def getFactValues(CIK, ticker, conceptName2):
 @app.callback(
     Output('hidden-div', "children"),
     [Input('btn-submit-sendPlotlyData', 'n_clicks')],
-    [State('datatable-factList', "rows"),
-     State('datatable-factList', "selected_row_indices")])
-def doSubmitSendPlotlyData(n_clicks, rows, selected_row_indices):
-    if (selected_row_indices != None and len(selected_row_indices) != 0):
+    [State('dt-factList', "derived_virtual_data"),
+     State('dt-factList', "derived_virtual_selected_rows")])
+def doSubmitSendPlotlyData(n_clicks, rows, selected_rows):
+    if (selected_rows != None and len(selected_rows) != 0):
         filterFactVOList = []
-        for selected_row in selected_row_indices:
+        for selected_row in selected_rows:
             filterFactVO = FilterFactVO()
             filterFactVO.conceptName = rows[selected_row]["conceptName"]
             filterFactVO.reportShortName = rows[selected_row]["reportName"]
@@ -120,4 +138,14 @@ def getNumberValueAsString(value):
     if(value % 1):
         return value
     else:
-        return str(int(value/1000000)) + " M" 
+        intLen = len(str(int(value)))
+        if(intLen < 4):
+            return value
+        elif(intLen < 7):
+            return str(int(value/1000)) + " m" 
+        elif(intLen >= 7):
+            return str(int(value/1000000)) + " M" 
+        # elif(intLen < 13):
+        #    return str(int(value/1000000000)) + " B"
+        else:
+            value
--- fundamentalAnalytics/web/apps/app2.py
+++ fundamentalAnalytics/web/apps/app2.py
@@ -5,20 +5,17 @@ Created on 4 nov. 2018
 '''
 import webbrowser
 
-import dash
 from dash.dependencies import Output, Input, State
 from pandas.core.frame import DataFrame
-
+import dash_table as dt
 from base.initializer import Initializer
 from dao.fileDataDao import FileDataDao
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
 from dataImport.importFactFromSEC import initMainCache
 from engine.factEngine import FactEngine
 from engine.factImporterEngine import FactImporterEngine
 from engine.importFileEngine import ImportFileEngine
-from valueobject.valueobject import FilterFactVO
 from web.app import app
 
 
@@ -27,8 +24,8 @@ Initializer()
 layout = html.Div([
     dcc.Input(id='txt-filename', value='', type='text'),
     html.Button(id='btn-submit', n_clicks=0, children='Submit'),
-    html.Div(dt.DataTable(rows=[{}], id='dt-fd2'), style={'display': 'none'}),
-    html.Div(id='dt-fd2-container'),
+    html.Div(dt.DataTable(data=[{}], id='dt-fileData'), style={'display': 'none'}),
+    html.Div(id='dt-fileData-container'),
     html.Button(id='btn-reimport', n_clicks=0, children='Reimport'),
     html.Button(id='btn-reprocess', n_clicks=0, children='Reprocess'),
     html.Button(id='btn-delete', n_clicks=0, children='Delete'),
@@ -41,7 +38,7 @@ layout = html.Div([
 ])
 
 @app.callback(
-    Output('dt-fd2-container', "children"),
+    Output('dt-fileData-container', "children"),
     [Input('btn-submit', 'n_clicks')],
     [State('txt-filename', "value")])
 def doSubmit(n_clicks, value):
@@ -50,70 +47,69 @@ def doSubmit(n_clicks, value):
         if (len(rs2) != 0):
             df2 = DataFrame(rs2)
             dt2 = dt.DataTable(
-                rows=df2.to_dict("rows"),
-                row_selectable=True,
-                filterable=True,
-                sortable=True,
-                selected_row_indices=[],
-                editable = False,
-                id='dt-fd2'
+                id='dt-fileData',
+                columns=[
+                    {"name": i, "id": i, "deletable": False} for i in df2.columns
+                ],
+                data=df2.to_dict("rows"),
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                row_selectable="multi",
                 ) 
             return dt2
 
 @app.callback(
     Output('hidden-div1', "children"),
     [Input('btn-reprocess', 'n_clicks')],
-    [State('dt-fd2', "rows"),
-     State('dt-fd2', "selected_row_indices")])
-def doReprocess(n_clicks, rows, selected_row_indices):
+    [State('dt-fileData', "derived_virtual_data"),
+     State('dt-fileData', "derived_virtual_selected_rows")])
+def doReprocess(n_clicks, rows, selected_rows):
     print(rows)
-    print(selected_row_indices)
-    if(selected_row_indices is not None and len(selected_row_indices) != 0):
+    print(selected_rows)
+    if(selected_rows is not None and len(selected_rows) != 0):
         mainCache = initMainCache()
-        fileName = rows[selected_row_indices[0]]["fileName"]
+        fileName = rows[selected_rows[0]]["fileName"]
+        print(fileName)
         fi = FactImporterEngine(fileName, True, mainCache)
         fi.doImport()
-
+ 
 @app.callback(
     Output('hidden-div2', "children"),
     [Input('btn-delete', 'n_clicks')],
-    [State('dt-fd2', "rows"),
-     State('dt-fd2', "selected_row_indices")])
-def doDelete(n_clicks, rows, selected_row_indices):
-    print(selected_row_indices)
-    if(selected_row_indices is not None and len(selected_row_indices) != 0):
-        fileName = rows[selected_row_indices[0]]["fileName"]
+    [State('dt-fileData', "derived_virtual_data"),
+     State('dt-fileData', "derived_virtual_selected_rows")])
+def doDelete(n_clicks, rows, selected_rows):
+    print(selected_rows)
+    if(selected_rows is not None and len(selected_rows) != 0):
+        fileName = rows[selected_rows[0]]["fileName"]
+        print(fileName)
         FactEngine.deleteFactByFileData(fileName)
-
-
+ 
+ 
 @app.callback(
     Output('hidden-div3', "children"),
     [Input('btn-reimport', 'n_clicks')],
-    [State('dt-fd2', "rows"),
-     State('dt-fd2', "selected_row_indices")])
-def doReImport(n_clicks, rows, selected_row_indices):
+    [State('dt-fileData', "derived_virtual_data"),
+     State('dt-fileData', "derived_virtual_selected_rows")])
+def doReImport(n_clicks, rows, selected_rows):
     print(rows)
-    print(selected_row_indices)
-    if(selected_row_indices is not None and len(selected_row_indices) != 0):
-        fileName = rows[selected_row_indices[0]]["fileName"]
+    print(selected_rows)
+    if(selected_rows is not None and len(selected_rows) != 0):
+        fileName = rows[selected_rows[0]]["fileName"]
+        print(fileName)
         ImportFileEngine.importFiles(filename=fileName, reimport=True)
-
+ 
 @app.callback(
     Output('hidden-div4', "children"),
     [Input('btn-goToSECURL', 'n_clicks')],
-    [State('dt-fd2', "rows"),
-     State('dt-fd2', "selected_row_indices")])
-def doGoToSECURL(n_clicks, rows, selected_row_indices):
+    [State('dt-fileData', "derived_virtual_data"),
+     State('dt-fileData', "derived_virtual_selected_rows")])
+def doGoToSECURL(n_clicks, rows, selected_rows):    
     print(rows)
-    print(selected_row_indices)
-    if(selected_row_indices is not None and len(selected_row_indices) != 0):
-        fileName = rows[selected_row_indices[0]]["fileName"]
+    print(selected_rows)
+    if(selected_rows is not None and len(selected_rows) != 0):
+        fileName = rows[selected_rows[0]]["fileName"]
         fileName = fileName.replace(".txt", "-index.htm")
         url = 'https://www.sec.gov/Archives/' + fileName
         webbrowser.open(url) 
-
-@app.callback(
-    Output('app-2-display-value', 'children'),
-    [Input('app-2-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
