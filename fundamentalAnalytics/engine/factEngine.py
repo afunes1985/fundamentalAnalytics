d923ca3a8ca34967ecc3d6e64e3772af0804473d
afunes <afunes@SISTEMAS044.ar.lumina.net>
2019-07-08 23:28:46 -0500

    BASE

--- fundamentalAnalytics/engine/factEngine.py
+++ fundamentalAnalytics/engine/factEngine.py
@@ -4,11 +4,9 @@ Created on 18 ago. 2018
 @author: afunes
 '''
 
-from sympy.parsing.sympy_parser import parse_expr
 
 from base.dbConnector import DBConnector
 from dao.dao import Dao, GenericDao
-from modelClass.customFactValue import CustomFactValue
 from modelClass.fileData import FileData
 from valueobject.constant import Constant
 
